a701ae79b0181d65e6f1e53eb47cbfd44b643de1
barry <barry.klein@student.moringaschool.com>
2023-06-01 12:48:14 +0300

    added a prompt

--- Create_data.py
+++ Create_data.py
@@ -2,12 +2,42 @@
 
 import sqlite3
 
+CREATE_BEANS_TABLE =  "CREATE TABLE iIF NOT EXIST beans (id INTEGER PRIMARY KEY, name TEXT, methord TEXT, rating INTEGER);" 
+
+INSERT_BEAN = "INSERT INTO bean (name, methord,rating) VALUE (?,?,?);"
+
+GET_ALL_BEAN ="SELECT * FROM BEAN;"
+GET_BEAN_BY_NAME = "SELECT*FROM beans WHERE name = ?;"
+GET_BEST_PREPARETION_FOR_BEAN =""""
+SELECT * FROM beans
+WHERE name = ?
+ORDER BY rating DEC
+LIMIT 1;
+"""
+
 def connect():
   return sqlite3.connect('data.db')
 
 def creat_tables(connection):
  with connection:
-    connection.execute(
-        "CREATE TABLE  (id INTEGER PRIMARY KEY, name TEXT, methord TEXT, rating INTEGER);" 
-    )
-    
+    connection.execute(CREATE_BEANS_TABLE)
+
+
+def add_bean(connection,name, methord, rating):
+   with connection:
+     connection.execute(INSERT_BEAN, (name,methord, rating))
+
+
+def get_all_beans(connection):
+ with connection:
+   return connection.execute(GET_ALL_BEAN).fetchall()
+
+
+
+def get_beans_by_name(connection, name):
+ with connection:
+   return connection.execute(GET_BEAN_BY_NAME).fetchall()
+
+def get_best_preparetion_for_bean(connection, name):
+  with connection:
+    return connection.execute (GET_BEST_PREPARETION_FOR_BEAN,(name,)).fetchone()
--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import Create_data
 
+MENU_PROMPT = """
+--Cofee Bean App --
+please chose one of these option:
+
+1)Add a new bean.
+2)See all beans.
+3)Find a bean by name.
+4)See which preparation methord is best for a bean.
+5)Exit.
+Your selection:"""
+
+
+
+
 def menu():
     connection = Create_data.connect()
-    Create_data.creat_table(connection)
+    Create_data.creat_tables(connection)
+
+    while (user_input := input(MENU_PROMPT)) != "5":
+        print(user_input)
+
+menu()
