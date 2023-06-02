bfac4e8f99c255b1b59a077becd1b64f68588e8a
barry <barry.klein@student.moringaschool.com>
2023-06-02 12:51:57 +0300

    change the structure

--- Create_data.py
+++ Create_data.py
@@ -1,43 +1,36 @@
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-CREATE_BEANS_TABLE = "CREATE TABLE IF NOT EXISTS beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);"
+Base = declarative_base()
 
-INSERT_BEAN = "INSERT INTO beans (name, method, rating) VALUES (?, ?, ?);"
-
-GET_ALL_BEANS = "SELECT * FROM beans;"
-GET_BEAN_BY_NAME = "SELECT * FROM beans WHERE name = ?;"
-GET_BEST_PREPARATION_FOR_BEAN = """
-SELECT * FROM beans
-WHERE name = ?
-ORDER BY rating DESC
-LIMIT 1;
-"""
-
-DELETE_BEAN = "DELETE FROM beans WHERE id = ?;"
+class Bean(Base):
+    __tablename__ = 'beans'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    method = Column(String)
+    rating = Column(Integer)
 
 def connect():
-    return sqlite3.connect('data.db')
+    engine = create_engine('sqlite:///data.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
 
-def create_tables(connection):
-    with connection:
-        connection.execute(CREATE_BEANS_TABLE)
+def add_bean(session, name, method, rating):
+    bean = Bean(name=name, method=method, rating=rating)
+    session.add(bean)
+    session.commit()
 
-def add_bean(connection, name, method, rating):
-    with connection:
-        connection.execute(INSERT_BEAN, (name, method, rating))
+def get_all_beans(session):
+    return session.query(Bean).all()
 
-def get_all_beans(connection):
-    with connection:
-        return connection.execute(GET_ALL_BEANS).fetchall()
+def get_beans_by_name(session, name):
+    return session.query(Bean).filter(Bean.name == name).all()
 
-def get_beans_by_name(connection, name):
-    with connection:
-        return connection.execute(GET_BEAN_BY_NAME, (name,)).fetchall()
+def get_best_preparation_for_bean(session, name):
+    return session.query(Bean).filter(Bean.name == name).order_by(Bean.rating.desc()).first()
 
-def get_best_preparation_for_bean(connection, name):
-    with connection:
-        return connection.execute(GET_BEST_PREPARATION_FOR_BEAN, (name,)).fetchone()
-
-def delete_bean(connection, bean_id):
-    with connection:
-        connection.execute(DELETE_BEAN, (bean_id,))
+def delete_bean(session, bean_id):
+    session.query(Bean).filter(Bean.id == bean_id).delete()
+    session.commit()
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import Create_data
 
 MENU_PROMPT = """
--- Coffee Bean App --
+--  BEANS IN KENYA  --
 Please choose one of these options:
 
 1) Add a new bean.
@@ -14,85 +14,86 @@ Your selection: """
 
 
 def menu():
-    connection = Create_data.connect()
-    Create_data.create_tables(connection)
+    session = Create_data.connect()
 
     while (user_input := input(MENU_PROMPT)) != "6":
         if user_input == "1":
-            prompt_add_new_bean(connection)
+            prompt_add_new_bean(session)
             
         elif user_input == "2":
-            prompt_see_all_beans(connection)
+            prompt_see_all_beans(session)
 
         elif user_input == "3":
-            prompt_find_bean(connection)
+            prompt_find_bean(session)
 
         elif user_input == "4":
-            prompt_find_best_method(connection)
+            prompt_find_best_method(session)
 
         elif user_input == "5":
-            prompt_delete_bean(connection)
+            prompt_delete_bean(session)
 
         else:
             print("INVALID INPUT. PLEASE TRY AGAIN.")
 
 
-def prompt_add_new_bean(connection):
+def prompt_add_new_bean(session):
     name = input("Enter bean name: ")
     method = input("Enter preparation method: ")
     rating = int(input("Enter rating score (0-100): "))
 
-    Create_data.add_bean(connection, name, method, rating)
+    Create_data.add_bean(session, name, method, rating)
     print("Bean added successfully!")
 
 
-def prompt_see_all_beans(connection):
-    beans = Create_data.get_all_beans(connection)
+def prompt_see_all_beans(session):
+    beans = Create_data.get_all_beans(session)
 
     if not beans:
         print("No beans found.")
     else:
         for bean in beans:
-            print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+            print(f"{bean.name} ({bean.method}) - {bean.rating}/100")
 
 
-def prompt_find_bean(connection):
+def prompt_find_bean(session):
     name = input("Enter bean name to find: ")
-    beans = Create_data.get_beans_by_name(connection, name)
+    beans = Create_data.get_beans_by_name(session, name)
 
     if not beans:
         print("No beans found.")
     else:
         for bean in beans:
-            print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+            print(f"{bean.name} ({bean.method}) - {bean.rating}/100")
 
 
-def prompt_find_best_method(connection):
+def prompt_find_best_method(session):
     name = input("Enter bean name to find: ")
-    best_method = Create_data.get_best_preparation_for_bean(connection, name)
+
+    
+    best_method = Create_data.get_best_preparation_for_bean(session, name)
 
     if not best_method:
         print("Bean not found.")
     else:
-        print(f"The best preparation method for {name} is: {best_method[2]}.")
+        print(f"The best preparation method for {name} is: {best_method.method}.")
 
 
-def prompt_delete_bean(connection):
+def prompt_delete_bean(session):
     name = input("Enter bean name to delete: ")
-    beans = Create_data.get_beans_by_name(connection, name)
+    beans = Create_data.get_beans_by_name(session, name)
 
     if not beans:
         print("Bean not found.")
     else:
         for index, bean in enumerate(beans, start=1):
-            print(f"{index}) {bean[1]} ({bean[2]}) - {bean[3]}/100")
+            print(f"{index}) {bean.name} ({bean.method}) - {bean.rating}/100")
 
         choice = input("Enter the number corresponding to the bean you want to delete: ")
         try:
             index = int(choice) - 1
             if 0 <= index < len(beans):
                 bean = beans[index]
-                Create_data.delete_bean(connection, bean[0])
+                Create_data.delete_bean(session, bean.id)
                 print("Bean deleted successfully!")
             else:
                 print("Invalid choice.")
