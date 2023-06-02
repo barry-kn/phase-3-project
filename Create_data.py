377dde2c9537ab3a54f17fde6bd7cae2fd37ed71
barry <barry.klein@student.moringaschool.com>
2023-06-02 15:29:44 +0300

    add famers-> create a table

--- Create_data.py
+++ Create_data.py
@@ -11,6 +11,13 @@ class Bean(Base):
     method = Column(String)
     rating = Column(Integer)
 
+class Farmer(Base):
+    __tablename__ = 'farmers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    area = Column(String)
+    phone = Column(Integer)
+
 def connect():
     engine = create_engine('sqlite:///data.db')
     Base.metadata.create_all(engine)
@@ -22,15 +29,24 @@ def add_bean(session, name, method, rating):
     session.add(bean)
     session.commit()
 
+def add_farmer(session, name, area, phone):
+    farmer = Farmer(name=name, area=area, phone=phone)
+    session.add(farmer)
+    session.commit()
+
 def get_all_beans(session):
     return session.query(Bean).all()
 
-def get_beans_by_name(session, name):
-    return session.query(Bean).filter(Bean.name == name).all()
+def find_bean_by_name(session, name):
+    return session.query(Bean).filter_by(name=name).first()
 
-def get_best_preparation_for_bean(session, name):
-    return session.query(Bean).filter(Bean.name == name).order_by(Bean.rating.desc()).first()
+def find_best_preparation_method(session, name):
+    bean = session.query(Bean).filter_by(name=name).first()
+    if bean:
+        return bean.method
+    else:
+        return None
 
-def delete_bean(session, bean_id):
-    session.query(Bean).filter(Bean.id == bean_id).delete()
+def delete_bean(session, bean):
+    session.delete(bean)
     session.commit()
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import Create_data
 
 MENU_PROMPT = """
---  BEANS IN KENYA  --
+-- Coffee Bean App --
 Please choose one of these options:
 
 1) Add a new bean.
@@ -9,14 +9,17 @@ Please choose one of these options:
 3) Find a bean by name.
 4) See which preparation method is best for a bean.
 5) Delete a bean.
-6) Exit.
+6) Add a new farmer.
+7) Exit.
 Your selection: """
 
 
 def menu():
     session = Create_data.connect()
 
-    while (user_input := input(MENU_PROMPT)) != "6":
+    while True:
+        user_input = input(MENU_PROMPT)
+
         if user_input == "1":
             prompt_add_new_bean(session)
             
@@ -32,6 +35,12 @@ def menu():
         elif user_input == "5":
             prompt_delete_bean(session)
 
+        elif user_input == "6":
+            prompt_add_new_farmer(session)
+
+        elif user_input == "7":
+            break
+
         else:
             print("INVALID INPUT. PLEASE TRY AGAIN.")
 
@@ -44,61 +53,55 @@ def prompt_add_new_bean(session):
     Create_data.add_bean(session, name, method, rating)
     print("Bean added successfully!")
 
+def prompt_add_new_farmer(session):
+    name = input("Enter farmer name: ")
+    area = input("Enter area: ")
+    while True:
+        phone = input("Enter phone number: ")
+        try:
+            phone = int(phone)
+            break
+        except ValueError:
+            print("Invalid phone number. Please enter a valid integer.")
+    Create_data.add_farmer(session, name, area, phone)
+    print("Farmer added successfully!")
+
 
 def prompt_see_all_beans(session):
     beans = Create_data.get_all_beans(session)
-
-    if not beans:
-        print("No beans found.")
-    else:
+    if beans:
+        print("List of all beans:")
         for bean in beans:
-            print(f"{bean.name} ({bean.method}) - {bean.rating}/100")
+            print(f"Name: {bean.name}, Method: {bean.method}, Rating: {bean.rating}")
+    else:
+        print("No beans found.")
 
 
 def prompt_find_bean(session):
-    name = input("Enter bean name to find: ")
-    beans = Create_data.get_beans_by_name(session, name)
-
-    if not beans:
-        print("No beans found.")
+    name = input("Enter bean name: ")
+    bean = Create_data.find_bean_by_name(session, name)
+    if bean:
+        print(f"Bean found - Name: {bean.name}, Method: {bean.method}, Rating: {bean.rating}")
     else:
-        for bean in beans:
-            print(f"{bean.name} ({bean.method}) - {bean.rating}/100")
+        print("Bean not found.")
 
 
 def prompt_find_best_method(session):
-    name = input("Enter bean name to find: ")
-
-    
-    best_method = Create_data.get_best_preparation_for_bean(session, name)
-
-    if not best_method:
-        print("Bean not found.")
+    name = input("Enter bean name: ")
+    method = Create_data.find_best_preparation_method(session, name)
+    if method:
+        print(f"The best preparation method for {name} is {method}.")
     else:
-        print(f"The best preparation method for {name} is: {best_method.method}.")
+        print("Bean not found.")
 
 
 def prompt_delete_bean(session):
-    name = input("Enter bean name to delete: ")
-    beans = Create_data.get_beans_by_name(session, name)
-
-    if not beans:
-        print("Bean not found.")
+    name = input("Enter bean name: ")
+    bean = Create_data.find_bean_by_name(session, name)
+    if bean:
+        Create_data.delete_bean(session, bean)
+        print(f"Bean {name} deleted successfully!")
     else:
-        for index, bean in enumerate(beans, start=1):
-            print(f"{index}) {bean.name} ({bean.method}) - {bean.rating}/100")
-
-        choice = input("Enter the number corresponding to the bean you want to delete: ")
-        try:
-            index = int(choice) - 1
-            if 0 <= index < len(beans):
-                bean = beans[index]
-                Create_data.delete_bean(session, bean.id)
-                print("Bean deleted successfully!")
-            else:
-                print("Invalid choice.")
-        except ValueError:
-            print("Invalid choice.")
-
+        print("Bean not found.")
 
 menu()
